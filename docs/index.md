f13d2715e87e6a519835b600b59df47f8ed1a0d7
Leah Wasser <leah@pyopensci.org>
2024-06-25 10:46:10 -0600

    Fix: use auto-api

--- docs/index.md
+++ docs/index.md
@@ -7,6 +7,7 @@
 home <self>
 hatch scripts <hatch-envs-scripts>
 Setup Sphinx <documentation/setup-sphinx>
+Reference / API <apidocs/index>
 :::
 
 :::{toctree}
--- src/pyospackage/add_numbers.py
+++ src/pyospackage/add_numbers.py
@@ -5,4 +5,18 @@ def add_num(
     a: int,
     b: int,
 ) -> int:
+    """A function that adds two  numbers together
+
+    Parameters
+    ----------
+    a : int
+        The first integer to be added.
+    b : int
+        The second integer to be added.
+
+    Returns
+    -------
+    int
+        The sum of the two provided integers.
+    """
     return a + b
